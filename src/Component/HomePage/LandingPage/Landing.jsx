dcc76401a9be04a4407808b89586a5a0a00fd533
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-02 12:28:04 +0530

    render data on page

--- src/Component/HomePage/LandingPage/Landing.jsx
+++ src/Component/HomePage/LandingPage/Landing.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { PropsPracticeParent } from "../../C-MainBodySection/SamplePage/PropsPracticeParent";
 
 export const Landing = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PropsPracticeParent />
+    </div>
+  );
 };
