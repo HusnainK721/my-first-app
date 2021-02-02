7d590975a87420ad5677664b7947e6d2e31a69b7
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-02 12:09:14 +0530

    Removed unnessesory code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import "./App.css";
-import { Header } from "./Component/HeaderSection/Header";
-import { MainBody } from "./Component/BodySection/MainBody";
-import AllForm from "./Component/FormComponent/AllForm";
+
 function App() {
-  return (
-    <div className="App">
-      {/* <Header />
-      <MainBody /> */}
-      <AllForm />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
