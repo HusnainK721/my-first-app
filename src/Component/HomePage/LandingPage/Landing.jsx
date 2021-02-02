d016038832f97033f71b2e97b1bcfac122e07aae
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-02 12:56:43 +0530

    Re-arrange the intire app folder

--- src/Component/HomePage/LandingPage/Landing.jsx
+++ src/Component/HomePage/LandingPage/Landing.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { PropsPracticeParent } from "../../C-MainBodySection/SamplePage/PropsPracticeParent";
 import { HeaderSection } from "../../A-HeaderSection/HeaderSection";
+import { LeftNavigationSection } from "../../B-LeftNavigationSection/LeftNavigationSection";
+import { MainBodySection } from "../../C-MainBodySection/MainBodySection";
+import { RightNavigationSection } from "../../D-RightNavigationSection/RightNavigationSection";
+import { FooterSection } from "../../E-FooterSection/FooterSection";
 export const Landing = () => {
   return (
     <div>
       <HeaderSection />
-      <PropsPracticeParent />
+      <LeftNavigationSection />
+      <MainBodySection />
+      <RightNavigationSection />
+      <FooterSection />
     </div>
   );
 };
