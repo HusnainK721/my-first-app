314b2096bf1e55c5e576aea9aee9393259676fda
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-03 16:11:43 +0530

    Working with class comp props

--- src/Component/C-MainBodySection/MainBodySection.jsx
+++ src/Component/C-MainBodySection/MainBodySection.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 // import { PropsWithClassParent } from "./PropsWithClassComponent/PropsWithClassParent";
 // import { PropsPracticeParent } from "./PropsWithFunctionComponent/PropsPracticeParent";
-import { PropsWithClassParent } from "./PropsWithClassComponent/PropsWithClassParent";
 export const MainBodySection = () => {
   return (
     <div>
       {/* <PropsPracticeParent /> */}
-      {/* <PropsWithClassParent /> */}
+      <PropsWithClassParent />
     </div>
   );
 };
