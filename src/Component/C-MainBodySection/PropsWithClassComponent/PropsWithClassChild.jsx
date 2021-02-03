64c5cf2118e0f5dbece1d38b4fcc81251f1f84d7
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-03 16:12:44 +0530

    Working with props in class comp

--- src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassChild.jsx
+++ src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassChild.jsx
@@ -1,19 +1,18 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// export class PropsWithClassChild extends Component {
-//   render() {
-//     return (
-//       <div>
-
-//         <h1>Props With Class Component</h1>
-//         <p>
-//           I am {this.props.name} i am {this.props.age} year old
-//         </p>
-//         {/* random number function detecting age from any number randomly */}
-//         {/* <h1>
-//           i am a person and i am {Math.floor(Math.random() * 30)} year old
-//         </h1> */}
-//       </div>
-//     );
-//   }
-// }
+export class PropsWithClassChild extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Props With Class Component</h1>
+        <p>
+          I am {this.props.name} i am {this.props.age} year old
+        </p>
+        {/* random number function detecting age from any number randomly */}
+        {/* <h1>
+          i am a person and i am {Math.floor(Math.random() * 30)} year old
+        </h1> */}
+      </div>
+    );
+  }
+}
