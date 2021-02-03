f409d431d44db8d88426f47cbc66cf7a3de7f8ba
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-03 16:10:18 +0530

    Removed extra code from propswithclassparent

--- src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
+++ src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
@@ -11,14 +11,3 @@
 //     );
 //   }
 // }
-import React, { Component } from "react";
-import PropsWithClassChild from "./PropsWithClassChild";
-export class PropsWithClassParent extends Component {
-  render() {
-    return (
-      <div>
-        <PropsWithClassChild />
-      </div>
-    );
-  }
-}
