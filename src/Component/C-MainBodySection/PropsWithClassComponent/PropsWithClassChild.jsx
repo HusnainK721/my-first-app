7b4f009910bf8a5bc6e2d95d8b463a8a0c16c110
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-03 16:09:31 +0530

    Removed extra code

--- src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassChild.jsx
+++ src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassChild.jsx
@@ -17,16 +17,3 @@
 //     );
 //   }
 // }
-
-import React, { Component } from "react";
-
-class PropsWithClassChild extends Component {
-  render() {
-    return (
-      <div>
-        <h1>This is class component</h1>
-      </div>
-    );
-  }
-}
-export default PropsWithClassChild;
