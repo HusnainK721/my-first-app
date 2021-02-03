ae362a8f95bc0993ab7d37c134d7ea8ae935a377
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-03 16:12:13 +0530

    Working with class comp props

--- src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
+++ src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
@@ -1,13 +1,13 @@
-// import React, { Component } from "react";
-// import { PropsWithClassChild } from "./PropsWithClassChild";
+import React, { Component } from "react";
+import { PropsWithClassChild } from "./PropsWithClassChild";
 
-// export class PropsWithClassParent extends Component {
-//   render() {
-//     return (
-//       <div>
-//         {/* <PropsWithClassChild name="Aamer Khan" age="28" /> */}
-//         {/* <PropsWithClassChild name="Aamer Khan" age="28" /> */}
-//       </div>
-//     );
-//   }
-// }
+export class PropsWithClassParent extends Component {
+  render() {
+    return (
+      <div>
+        {/* <PropsWithClassChild name="Aamer Khan" age="28" /> */}
+        {/* <PropsWithClassChild name="Aamer Khan" age="28" /> */}
+      </div>
+    );
+  }
+}
