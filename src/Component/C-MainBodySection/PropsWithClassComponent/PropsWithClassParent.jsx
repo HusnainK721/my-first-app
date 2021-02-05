59c8410ee5f38ed519be6d59ecca6f702d2a5c3d
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-05 16:33:51 +0530

    created headerSection css file

--- src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
+++ src/Component/C-MainBodySection/PropsWithClassComponent/PropsWithClassParent.jsx
@@ -10,20 +10,31 @@ export class PropsWithClassParent extends Component {
       { name: "Captain America", age: 56 },
     ],
   };
+
   // onClick of button it will execute & Changing the existing state to new state
-  changeNameHandler = () => {
+  // changeNameHandler = () => {
+  //   this.setState({
+  //     person: [
+  //       { name: "Bruce", age: 25 },
+  //       { name: "Clark", age: 30 },
+  //       { name: "Steve Rogers", age: 200 },
+  //     ],
+  //   });
+  // };
+  switchNameHandler = () => {
     this.setState({
       person: [
-        { name: "Bruce", age: 25 },
-        { name: "Clark", age: 30 },
-        { name: "Steve Rogers", age: 200 },
+        { name: "Iron Man", age: 25 },
+        { name: "Spider Man", age: 22 },
+        { name: "Hulk", age: 35 },
       ],
     });
   };
+
   render() {
     return (
       <div>
-        <button onClick={this.changeNameHandler}>On Click Event</button>
+        <button onClick={this.switchNameHandler}>On Click Event</button>
         {/* Passing state as props to child component */}
         <PropsWithClassChild
           name={this.state.person[0].name}
