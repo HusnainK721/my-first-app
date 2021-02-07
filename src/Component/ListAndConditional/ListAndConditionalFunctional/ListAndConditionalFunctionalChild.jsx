869edb3b8f920538c17e8c829d2197b30370eedd
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-08 00:04:58 +0530

    Conditional rendering in functional component

--- src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalChild.jsx
+++ src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalChild.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const ListAndConditionalFunctionalChild = () => {
+export const ListAndConditionalFunctionalChild = (props) => {
   return (
     <div>
-      <p>hiiii</p>
+      <div>
+        <p onClick={props.clicked}>
+          I am {props.name} and i am {props.age} year old
+        </p>
+        <input type="text" onChange={props.change} />
+      </div>
     </div>
   );
 };
--- src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalParent.jsx
+++ src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalParent.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListAndConditionalFunctionalChild } from "./ListAndConditionalFunctionalChild";
 
 export const ListAndConditionalFunctionalParent = () => {
+  const [showPerson, setShowPerson] = useState(false);
+  const [personData, setPersonData] = useState({
+    person: [
+      { name: "Salman", age: 23 },
+      { name: "Khuddus", age: 24 },
+      { name: "Zeeshan", age: 25 },
+    ],
+  });
+  const changeNameHandler = () => {
+    setPersonData({
+      person: [
+        { name: "Node", age: 23 },
+        { name: "Backend", age: 24 },
+        { name: "UI", age: 25 },
+      ],
+    });
+  };
+  const togglePersonHandler = () => {
+    setShowPerson(!showPerson);
+  };
   return (
     <div>
-      <ListAndConditionalFunctionalChild />
+      <button onClick={togglePersonHandler}>Click TO Change State 1</button>
+      {showPerson ? (
+        <div>
+          <ListAndConditionalFunctionalChild
+            name={personData.person[0].name}
+            age={personData.person[0].age}
+          />
+          <ListAndConditionalFunctionalChild
+            name={personData.person[1].name}
+            age={personData.person[1].age}
+          />
+          <ListAndConditionalFunctionalChild
+            name={personData.person[2].name}
+            age={personData.person[2].age}
+            // change={changeNameHandler}
+          />
+          <button onClick={changeNameHandler}>Click TO Change State 2</button>
+        </div>
+      ) : null}
     </div>
   );
 };
