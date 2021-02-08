dd08c28f1ffe3b7353a4c780961877133207a02a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-08 19:28:02 +0530

    working with conditional rendering

--- src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalParent.jsx
+++ src/Component/ListAndConditional/ListAndConditionalFunctional/ListAndConditionalFunctionalParent.jsx
@@ -59,14 +59,53 @@
 
 //***Conditonal rendering in javaScript way***
 import React, { useState } from "react";
-import { ToggleButton } from "react-bootstrap";
+import { ListAndConditionalFunctionalChild } from "./ListAndConditionalFunctionalChild";
 
 export const ListAndConditionalFunctionalParent = () => {
-  const [show, setShow] = useState(false);
-  const [personData, setPersonData] = useState({});
-  const TogglePersonHandler = () => {};
+  const [show, setShow] = useState();
+  const [personData, setPersonData] = useState({
+    person: [
+      { name: "Salman", age: 23 },
+      { name: "Khuddus", age: 24 },
+      { name: "Zeeshan", age: 25 },
+    ],
+  });
+  const switchNameHandler = () => {
+    setPersonData({
+      person: [
+        { name: "Salman khan", age: 23 },
+        { name: "Khuddus shaikh", age: 24 },
+        { name: "Zeeshan gazi", age: 25 },
+      ],
+    });
+  };
+  const TogglePersonHandler = () => {
+    const isShow = personData;
+    setShow(!isShow);
+  };
+  let Person = null;
+  if (show) {
+    Person = (
+      <div>
+        <ListAndConditionalFunctionalChild
+          name={personData.person[0].name}
+          age={personData.person[0].age}
+        />
+        <ListAndConditionalFunctionalChild
+          name={personData.person[1].name}
+          age={personData.person[1].age}
+        />
+        <ListAndConditionalFunctionalChild
+          name={personData.person[2].name}
+          age={personData.person[2].age}
+        />
+        <button onClick={switchNameHandler}>change state</button>
+      </div>
+    );
+  }
   return (
     <div>
+      {Person}
       <button onClick={TogglePersonHandler}>
         Conditional Rendering In js way
       </button>
