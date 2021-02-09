95df7054d15ae4afa4930499f4b27b43c0b0fc83
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-09 21:05:20 +0530

    working on deleting existing state

---
+++ src/Component/Module-4-ListAndConditional/ListAndConditionalClass/D-DeletingState/DeletingStateChildC.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class DeletingStateChildC extends Component {
+  render() {
+    return (
+      <div>
+        <p>hiiiiiiiiiiiii</p>
+      </div>
+    );
+  }
+}
