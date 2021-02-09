5ddbd2a4c8781df97347913954285ad6ec16f389
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-09 19:38:16 +0530

    Working with map function

--- src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/C-OutputtingListDynamicUsingMapF/OutputtingListDynamicallyChildF.jsx
+++ src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/C-OutputtingListDynamicUsingMapF/OutputtingListDynamicallyChildF.jsx
@@ -7,5 +7,3 @@ export const OutputtingListDynamicallyChildF = () => {
     </div>
   );
 };
-
-export default OutputtingListDynamicallyChildF;
--- src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/C-OutputtingListDynamicUsingMapF/OutputtingListDynamicallyParentF.jsx
+++ src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/C-OutputtingListDynamicUsingMapF/OutputtingListDynamicallyParentF.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { OutputtingListDynamicallyChildF } from "./OutputtingListDynamicallyChildF";
 
 const OutputtingListDynamicallyParentF = () => {
-  return <div></div>;
+  return (
+    <div>
+      <OutputtingListDynamicallyChildF />
+    </div>
+  );
 };
 
 export default OutputtingListDynamicallyParentF;
