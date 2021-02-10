0b910dc5ba77776bf734508592b836afba21b184
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-10 14:56:12 +0530

    working with delete state

--- src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/D-DeletingState/DeletingStateParentF.jsx
+++ src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/D-DeletingState/DeletingStateParentF.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { DeletingStateChildF } from "./DeletingStateChildF";
 export const DeletingStateParentF = () => {
+  const [personData, setPersonData] = useState({
+    person: [
+      { name: "khwaja", age: 27 },
+      { name: "Zahoor", age: 18 },
+      { name: "Rehan", age: 20 },
+    ],
+  });
   return (
     <div>
+      {personData.person.map}
       <DeletingStateChildF />
     </div>
   );
