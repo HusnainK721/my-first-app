45115b619d55aa335d18a605058417092d001adf
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-11 22:31:41 +0530

    removed onClick of paragraph in child

--- src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/D-DeletingState/DeletingStateChildF.jsx
+++ src/Component/Module-4-ListAndConditional/ListAndConditionalFunctional/D-DeletingState/DeletingStateChildF.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export const DeletingStateChildF = (itemCurrent) => {
   return (
     <div>
-      <p onClick={itemCurrent.clicked}>
-        my name is {itemCurrent.name} & i am 27 year old
-      </p>
+      <p>my name is {itemCurrent.name} & i am 27 year old</p>
       <input type="text" />
     </div>
   );
