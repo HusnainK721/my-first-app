2490f93c687bd7368738237fb73d5ff5f219fb85
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-12 16:27:14 +0530

    working with map function

--- src/Component/Practice/MapFunctionaPractice/MapFunctionPracticeChild.jsx
+++ src/Component/Practice/MapFunctionaPractice/MapFunctionPracticeChild.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export const MapFunctionPracticeChild = () => {
   return (
     <div>
-      <p>this is khwaja i am 27 year old</p>
+      <p>
+        this is khwaja i am 27 year old!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      </p>
     </div>
   );
 };
--- src/Component/Practice/MapFunctionaPractice/MapFunctionPracticeParent.jsx
+++ src/Component/Practice/MapFunctionaPractice/MapFunctionPracticeParent.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapFunctionPracticeChild } from "./MapFunctionPracticeChild";
 
 export const MapFunctionPracticeParent = () => {
+  const PersonData = [
+    { Company: "Pickupbiz", carName: "tesla" },
+    { Company: "Ford", carName: "indigo" },
+    { Company: "Tata", carName: "Nano" },
+    { Company: "audi", carName: "audix1" },
+    { Company: "mahindra", carName: "Thaar" },
+  ];
+
+  const [personDetails, setPersonDetails] = useState(PersonData);
   return (
     <div>
-      <MapFunctionPracticeChild />
+      {personDetails.map((fetchedData) => {
+        return <MapFunctionPracticeChild />;
+      })}
     </div>
   );
 };
