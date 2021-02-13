1bf483410ad8ca7dd3aab343bf2619e5d17aefd3
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-13 15:45:15 +0530

    working with json

--- src/Component/State/Reducers/PersonReducer.js
+++ src/Component/State/Reducers/PersonReducer.js
@@ -1,13 +1,53 @@
 const PersonReducers = {
   personDetail: [
-    { name: "Sam", age: 20, description: "He is a 1st year student" },
-    { name: "Mazhar", age: 21, description: `He is a 2nr year student` },
-    { name: "Rizwan", age: 22, description: `He is a 3rd year student` },
-    { name: "Aadil", age: 23, description: `He is a 12th student` },
-    { name: "Zahoor", age: 24, description: `He is a 11th student` },
-    { name: "Mujahed", age: 25, description: `He is a 10th student` },
-    { name: "Kaleem", age: 26, description: `He is a 9th student` },
-    { name: "Zubair", age: 27, description: `He is a 8th student` },
-    { name: "Aamer", age: 28, description: `He is a 7th student` },
+    {
+      id: "1asdf",
+      name: "Sam",
+      age: 20,
+      description: "He is a 1st year student",
+    },
+    {
+      id: "2asdf",
+      name: "Mazhar",
+      age: 21,
+      description: `He is a 2nr year student`,
+    },
+    {
+      id: "3asdf",
+      name: "Rizwan",
+      age: 22,
+      description: `He is a 3rd year student`,
+    },
+    {
+      id: "4asdf",
+      name: "Aadil",
+      age: 23,
+      description: `He is a 12th student`,
+    },
+    {
+      id: "5asdf",
+      name: "Zahoor",
+      age: 24,
+      description: `He is a 11th student`,
+    },
+    {
+      id: "6asdf",
+      name: "Mujahed",
+      age: 25,
+      description: `He is a 10th student`,
+    },
+    {
+      id: "7asdf",
+      name: "Kaleem",
+      age: 26,
+      description: `He is a 9th student`,
+    },
+    {
+      id: "8asdf",
+      name: "Zubair",
+      age: 27,
+      description: `He is a 8th student`,
+    },
+    { id: "9asdf", name: "Aamer", age: 28, description: `He is a 7th student` },
   ],
 };
