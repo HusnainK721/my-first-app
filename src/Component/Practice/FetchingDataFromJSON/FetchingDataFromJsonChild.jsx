412a6806230926b41ae1ccca63cefc6aab0c523f
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-13 16:15:22 +0530

    working with dynamic loading

--- src/Component/Practice/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
+++ src/Component/Practice/FetchingDataFromJSON/FetchingDataFromJsonChild.jsx
@@ -6,12 +6,9 @@ export const FetchingDataFromJsonChild = () => {
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="Assets/Rose.jpg" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Title>Red Rose</Card.Title>
+          <Card.Text>This is red rose tommorow is valentins day</Card.Text>
+          <Button variant="primary">Click Me</Button>
         </Card.Body>
       </Card>
     </div>
