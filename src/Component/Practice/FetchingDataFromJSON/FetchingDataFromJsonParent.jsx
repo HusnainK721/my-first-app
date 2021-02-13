3fc7915be27a24fd7498da4a8b0de2cf20861848
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-13 16:23:08 +0530

    successfully imported data from json

--- src/Component/Practice/FetchingDataFromJSON/FetchingDataFromJsonParent.jsx
+++ src/Component/Practice/FetchingDataFromJSON/FetchingDataFromJsonParent.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FetchingDataFromJsonChild } from "./FetchingDataFromJsonChild";
+import { PersonReducers } from "../../State/Reducers/PersonReducer";
 export const FetchingDataFromJsonParent = () => {
+  console.log("testing personReducer=>=>=>=>", PersonReducers);
   return (
     <div>
+      {}
       <FetchingDataFromJsonChild />
     </div>
   );
