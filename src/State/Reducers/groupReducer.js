bac0b6593564094acb0698113e2647c51662ec88
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-16 15:55:35 +0530

    working with reducers

---
+++ src/State/Reducers/groupReducer.js
@@ -0,0 +1,21 @@
+const groupReducer = () => {};
+
+const groupsDefaultState = {
+  groups: [
+    {
+      name: "Redux Practical",
+      image: "Assets/Rose.jpg",
+      desc: "This Is First Group Created With Redux",
+    },
+    {
+      name: "Learning Redux",
+      image: "Assets/Rose.jpg",
+      desc: "This Is First Group Created With Redux",
+    },
+    {
+      name: "React Developers",
+      image: "logo512.png",
+      desc: "This Is First Group Created With Redux",
+    },
+  ],
+};
