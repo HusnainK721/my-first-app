778246adbfb01a9dfa23603adad918e229b99126
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-16 16:15:02 +0530

    working with reducers

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -1,6 +1,7 @@
-const groupReducer = (state = groupsDefaultState, action) => {
+export const groupReducer = (state = groupsDefaultState, action) => {
   switch (action.type) {
     case "ADD_NEW_GROUP":
+      return { ...state, groups: [...state.groups, action.groups] };
   }
 };
 
