88441241043e1299bcd56a4d264c6c96706b0583
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-18 18:06:02 +0530

    set group data using useEffect

--- src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
+++ src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FetchingDataFromReduxChild } from "./FetchingDataFromReduxChild";
 import { useSelector } from "react-redux";
 import { getAllGroups } from "../../../State/Selector/groupSelector";
 export const FetchingDataFromReduxParent = () => {
   const selector = useSelector(getAllGroups);
-  const [groupData, setGroupData] = useState(selector);
+  const [groupData, setGroupData] = useState();
+  useEffect(() => {
+    setGroupData(selector);
+  }, [selector]);
 
   return (
     <div>
