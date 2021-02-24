4a914f32358b5dab47bbcfb779befc948003b17a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-24 16:19:04 +0530

    creating new state

---
+++ src/State/Reducers/userPostReducer.js
@@ -0,0 +1,39 @@
+const postDefaultState = {
+  // OR initialState
+  posts: [
+    {
+      id: uuidv4(),
+      name: "Rabey",
+      img: "URL",
+      description:
+        "sjfjfgsjfgjfgsdjfgssjhfgjfgsjhfgsjhfgsjhfgjhfgasfgfgaslhfgashfgsf",
+    },
+    {
+      id: uuidv4(),
+      name: "Rizwan",
+      img: "URL",
+      description:
+        "sjfjfgsjfgjfgsdjfgssjhfgjfgsjhfgsjhfgsjhfgjhfgasfgfgaslhfgashfgsf",
+    },
+    {
+      id: uuidv4(),
+      name: "Aamer",
+      img: "URL",
+      description:
+        "sjfjfgsjfgjfgsdjfgssjhfgjfgsjhfgsjhfgsjhfgjhfgasfgfgaslhfgashfgsf",
+    },
+  ],
+};
+
+export const userPostReducer = (state = postDefaultState, action) => {
+  console.log("sdjfgsjfgsj", state);
+  switch (action.type) {
+    case "ADD_NEW_POST":
+      return {
+        ...state,
+        posts: [...state.posts, action.posts],
+      };
+    default:
+      return state;
+  }
+};
