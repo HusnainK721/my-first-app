3f1251673fb3d28d4a2d909a910f0cae30155e70
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-25 19:42:02 +0530

    removed extra code

--- src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
+++ src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
@@ -34,41 +34,6 @@ export const FetchingDataFromReduxParent = () => {
               <FetchingDataFromReduxChild itemCurrent={item} key={item.id} />
             );
           })}
-
-        {/* <table border="10">
-        <tr>
-          <th>SR No</th>
-          <th>name</th>
-          <th>Mobile No</th>
-        </tr>
-
-        {groupData &&
-          groupData.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>{item.Rollno}</td>
-                <td>{item.name}</td>
-                <td>{item.contact}</td>
-                <td> </td>
-              </tr>
-            );
-          })}
-      </table>
-
-      {postData &&
-        postData.map((item) => {
-          console.log("data==>", item);
-          return (
-            <div key={item.id}>
-              <li>{item.name}</li>
-              <li>
-                {" "}
-                <img src={item.img} alt="" />
-              </li>
-              <li>{item.description}</li>
-            </div>
-          );
-        })} */}
       </div>
     </div>
   );
