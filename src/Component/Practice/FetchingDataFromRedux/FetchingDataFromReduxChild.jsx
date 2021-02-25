c76d26b669a4f7ce6f39d3d9ddfdb471aed79d16
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-25 16:34:12 +0530

    initial checkin

--- src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxChild.jsx
+++ src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxChild.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FetchingDataFromReduxChild = ({ itemCurrent }) => {
   return (
-    <div>
+    <div style={{ display: "inline-block", padding: "30px" }}>
       <h1>{itemCurrent.name}</h1>
       <img src={itemCurrent.image} alt="images" width="200px" height="200px" />
     </div>
