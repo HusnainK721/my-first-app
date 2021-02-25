8fb38aaf083bc61204bb76a9c8e1200c379e830c
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-02-25 17:11:13 +0530

    added action

--- src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
+++ src/Component/Practice/FetchingDataFromRedux/FetchingDataFromReduxParent.jsx
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from "react";
 import { FetchingDataFromReduxChild } from "./FetchingDataFromReduxChild";
 import { useSelector } from "react-redux";
 import { getAllGroups } from "../../../State/Selector/groupSelector";
+import { useDispatch } from "react-redux";
+import { addNewGroup } from "../../../State/Action/groupActions";
 export const FetchingDataFromReduxParent = () => {
   // const [postData, setPostData] = useState();
   const [groupData, setGroupData] = useState();
-  const [addNewGroup, setAddNewGroup] = useState();
+  const [addNewGroups, setAddNewGroups] = useState();
   const selector = useSelector(getAllGroups);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setGroupData(selector);
   }, [selector]);
+
+  const handleNewGroup = () => {
+    dispatch(addNewGroup(addNewGroups));
+  };
   return (
     <div>
       <div className="mt-2">
-        <input type="text" /> <button>Add Group</button>
+        <input
+          type="text"
+          onChange={(event) => setAddNewGroups(event.target.value)}
+        />
+        <button onClick={handleNewGroup}>Add Group</button>
       </div>
       <div>
         {groupData &&
