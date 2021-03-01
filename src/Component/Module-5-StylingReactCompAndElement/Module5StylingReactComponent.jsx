477bc4ffd6b409fa8b3a30868963f5c4c0441498
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-01 20:11:27 +0530

    dynamic loding className in class Comp

--- src/Component/Module-5-StylingReactCompAndElement/Module5StylingReactComponent.jsx
+++ src/Component/Module-5-StylingReactCompAndElement/Module5StylingReactComponent.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+// Class component exported here
 import { SettingStyleDynamicallyParentC } from "./StylingReactInClass/A-SettingStyleDynamicC/SettingStyleDynamicallyParentC";
+import { SettingClassNameDynamicallyParentC } from "./StylingReactInClass/B-SettingClassNameDynamicallyC/SettingClassNameDynamicallyParentC";
+// Functional component exported here
+
 import { SettingStyleDynamicallyParentF } from "./StylingReactInFunctional/A-SettingStyleDynamicallyF/SettingStyleDynamicallyParentF";
 export const Module5StylingReactComponent = () => {
   return (
@@ -11,6 +15,7 @@ export const Module5StylingReactComponent = () => {
           <div>
             <h4>Module 5 Using Class Component</h4>
             <SettingStyleDynamicallyParentC />
+            <SettingClassNameDynamicallyParentC />
           </div>
         </Col>
         <Col>
---
+++ src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/B-SettingClassNameDynamicallyC/SettingClassNameDynamicallyChildC.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export class SettingClassNameDynamicallyChildC extends Component {
+  render() {
+    return (
+      <div>
+        <p onClick={this.props.clicked}>
+          I am {this.props.name} i am {this.props.age} year old
+        </p>
+        <input
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
