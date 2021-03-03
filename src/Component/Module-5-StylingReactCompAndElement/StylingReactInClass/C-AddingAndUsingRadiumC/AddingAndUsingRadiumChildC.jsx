051b70f1b5eb89645ec5e192745b8627ce67833a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-03 13:10:09 +0530

    updated child component

--- src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumChildC.jsx
+++ src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumChildC.jsx
@@ -2,8 +2,18 @@ import React, { Component } from "react";
 
 class AddingAndUsingRadiumChildC extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p onClick={this.props.delete}>
+          I am {this.props.name} i am {this.props.age} year old
+        </p>
+        <input
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name}
+        />
+      </div>
+    );
   }
 }
-
 export default AddingAndUsingRadiumChildC;
