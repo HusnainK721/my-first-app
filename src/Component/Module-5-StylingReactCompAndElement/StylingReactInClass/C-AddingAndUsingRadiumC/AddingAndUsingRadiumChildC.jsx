df7f87e061b1750fdaaab0c1c62fb080cd0c721a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-03 13:32:18 +0530

    working with radium

--- src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumChildC.jsx
+++ src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumChildC.jsx
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 
 class AddingAndUsingRadiumChildC extends Component {
   render() {
+    const divStyle = {
+      '@media(min-width:"500px")': {
+        width: "350px",
+      },
+    };
     return (
-      <div>
+      <div style={divStyle}>
         <p onClick={this.props.delete}>
           I am {this.props.name} i am {this.props.age} year old
         </p>
