8c0ee7b6fe9e62ac62fd32a1e42e07b897ab8571
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-03 13:08:00 +0530

    working with radium

--- src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumParentC.jsx
+++ src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumParentC.jsx
@@ -29,12 +29,6 @@ class AddingAndUsingRadiumParentC extends Component {
   };
   deleteStateHandler = (personDataIndex) => {
     const personData = this.state.person;
-    // instead of above line you can write as follow:-
-    // 1. const personData= this.state.person.slice()===>>> it will copy original array & make changes in copied data
-    // OR
-    // 2. const personData=[...this.state.person]===>> this is latest feature of copying data (spread operator)
-    // this two method is called state immutability (copying existing state to new variable)
-    // now you are deleting the state from copied state instead of original state
     personData.splice(personDataIndex, 1);
     this.setState({ personData: personData });
   };
