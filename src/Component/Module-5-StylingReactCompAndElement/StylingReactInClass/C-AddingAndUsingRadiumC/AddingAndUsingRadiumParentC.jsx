00103b085e96a58252156683cb3c46c0431ea0f7
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-03 12:58:32 +0530

    created parent in radium componnt

---
+++ src/Component/Module-5-StylingReactCompAndElement/StylingReactInClass/C-AddingAndUsingRadiumC/AddingAndUsingRadiumParentC.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AddingAndUsingRadiumParentC extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default AddingAndUsingRadiumParentC;
