39df78aff1a72f9ac713b46d7e3a269afb34ced9
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-05 19:11:22 +0530

    managing code

--- src/Component/HomePage/LandingPage/Landing.jsx
+++ src/Component/HomePage/LandingPage/Landing.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { HeaderSection } from "../../A-HeaderSection/HeaderSection";
-import { LeftNavigationSection } from "../../B-LeftNavigationSection/LeftNavigationSection";
+// import { HeaderSection } from "../../A-HeaderSection/HeaderSection";
+// import { LeftNavigationSection } from "../../B-LeftNavigationSection/LeftNavigationSection";
 // import { MainBodySection } from "../../C-MainBodySection/MainBodySection";
-import { RightNavigationSection } from "../../D-RightNavigationSection/RightNavigationSection";
-import { FooterSection } from "../../E-FooterSection/FooterSection";
+// import { RightNavigationSection } from "../../D-RightNavigationSection/RightNavigationSection";
+// import { FooterSection } from "../../E-FooterSection/FooterSection";
 //all module 4 exported here
 import { Module4ListAndConditional } from "../../Module-4-ListAndConditional/Module4ListAndConditional";
 // all module 5 exported here
@@ -11,34 +11,28 @@ import { Module5StylingReactComponent } from "../../Module-5-StylingReactCompAnd
 
 // All Practice Component Exported Here
 import AllPractice from "../../Practice/AllPractice";
-// import { CounterPractice } from "../../Practice/UseStatePractice/CounterPractice/CounterPractice";
-// import { FetchingDataFromReduxParent } from "../../Practice/FetchingDataFromRedux/FetchingDataFromReduxParent";
-// import { MapFunctionPracticeParent } from "../../Practice/MapFunctionaPractice/MapFunctionPracticeParent";
-import RadiumPracticeParent from "../../Practice/RadiumPractice/RadiumPracticeParent";
+
 export const Landing = () => {
   return (
     <React.Fragment>
       {/* <div>
-        <h1>React Class Practice</h1>
-        <MapFunctionPracticeParent />
-        <FetchingDataFromReduxParent />
-      </div> */}
-
-      <div>
         <HeaderSection />
         <LeftNavigationSection />
-        {/* <MainBodySection /> */}
+        <MainBodySection />
         <RightNavigationSection />
         <FooterSection />
-        {/* <CounterPractice /> */}
+      </div> */}
+      <div>
+        <AllPractice />
       </div>
-      {/* <div>
-        <Module4ListAndConditional />
-      </div>*/}
       <hr />
-      <div>{/* <Module5StylingReactComponent /> */}</div>
-      <AllPractice />
-      {/* <RadiumPracticeParent /> */}
+      <div>
+        <Module4ListAndConditional />
+      </div>
+      <hr />
+      <div>
+        <Module5StylingReactComponent />
+      </div>
     </React.Fragment>
   );
 };
--- src/Component/Practice/AllPractice.jsx
+++ src/Component/Practice/AllPractice.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import StyledComponentparent from "./StyledComponent/StyledComponentparent";
-
+import { CounterPractice } from "./UseStatePractice/CounterPractice/CounterPractice";
+import { FetchingDataFromReduxParent } from "./FetchingDataFromRedux/FetchingDataFromReduxParent";
+import { MapFunctionPracticeParent } from "./MapFunctionaPractice/MapFunctionPracticeParent";
+import RadiumPracticeParent from "./RadiumPractice//RadiumPracticeParent";
 const AllPractice = () => {
   return (
     <div>
-      <StyledComponentparent />
+      <div>
+        <h1>React Class Practice</h1>
+        <h4>Counter Practice</h4>
+        <CounterPractice />
+        <MapFunctionPracticeParent />
+        <FetchingDataFromReduxParent />
+        <RadiumPracticeParent />
+        <StyledComponentparent />
+      </div>
     </div>
   );
 };
