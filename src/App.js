351532ad7847556821b4fe11451288d8ef2b5d33
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-31 23:42:47 +0530

    onClick changing css from external css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import { Landing } from "./Component/HomePage/LandingPage/Landing";
 function App() {
+  const [button, setButton] = useState();
+  const handleClick = () => {
+    setButton(!button);
+  };
   return (
     <div>
       <div className="App">
         <Landing />
+      </div>
+      <div>
+        <button
+          className={button ? "buttonTrue" : "buttonFalse"}
+          onClick={handleClick}
+        >
+          onCick Changing Style
+        </button>
       </div>
     </div>
   );
